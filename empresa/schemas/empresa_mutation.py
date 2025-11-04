8d1029308a61a8e4447d101ca4dbb9579532374d
FragmentosTemporales <cristian.rivera3284@gmail.com>
2025-11-03 21:02:56 -0300

    validaciones

--- empresa/schemas/empresa_mutation.py
+++ empresa/schemas/empresa_mutation.py
@@ -3,7 +3,30 @@ from empresa.models import Empresa
 from empresa.schemas.schema_model import EmpresaType
 
 
-class CreateEmpresa(graphene.Mutation):
+class BaseEmpresa:
+
+    @staticmethod
+    def validate_run(run):
+        lower_run = str(run.lower())
+        if '-' not in lower_run:
+            raise Exception("El RUN debe contener un guion (-)")
+        # Aquí puedes agregar la lógica para validar el RUN
+        elif len(lower_run) < 9:
+            raise Exception("El RUN es demasiado corto.")
+        elif len(lower_run) > 20:
+            raise Exception("El RUN es demasiado largo.")
+        return True
+
+    @staticmethod
+    def validate_nombre(nombre):
+        if len(nombre) < 3:
+            raise Exception("El nombre debe tener al menos 3 caracteres.")
+        elif len(nombre) > 255:
+            raise Exception("El nombre es demasiado largo.")
+        return True
+
+
+class CreateEmpresa(graphene.Mutation, BaseEmpresa):
     class Arguments:
         nombre = graphene.String(required=True)
         run = graphene.String(required=True)
@@ -13,17 +36,25 @@ class CreateEmpresa(graphene.Mutation):
     empresa = graphene.Field(EmpresaType)
 
     def mutate(self, info, nombre, run, direccion, website):
-        empresa = Empresa(
-            nombre=nombre,
-            run=run,
-            direccion=direccion,
-            website=website
-        )
-        empresa.save()
-        return CreateEmpresa(empresa=empresa)
+        try:
+
+            CreateEmpresa.validate_run(run)
+            CreateEmpresa.validate_nombre(nombre)
+
+            empresa = Empresa(
+                nombre=nombre,
+                run=run,
+                direccion=direccion,
+                website=website
+            )
+            empresa.save()
+
+            return CreateEmpresa(empresa=empresa)
+        except Exception as e:
+            raise Exception(f"Error al crear la empresa: {str(e)}")
 
 
-class DeleteEmpresa(graphene.Mutation):
+class DeleteEmpresa(graphene.Mutation, BaseEmpresa):
 
     class Arguments:
         id = graphene.ID(required=True)
@@ -39,7 +70,7 @@ class DeleteEmpresa(graphene.Mutation):
             return DeleteEmpresa(message="Empresa no encontrada.")
 
 
-class UpdateEmpresa(graphene.Mutation):
+class UpdateEmpresa(graphene.Mutation, BaseEmpresa):
 
     class Arguments:
         id = graphene.ID(required=True)
@@ -50,11 +81,19 @@ class UpdateEmpresa(graphene.Mutation):
 
     empresa = graphene.Field(EmpresaType)
 
-    def mutate(self, info, id, nombre):
-        empresa = Empresa.objects.get(pk=id)
-        empresa.nombre = nombre
-        empresa.save()
-        return UpdateEmpresa(empresa=empresa)
+    def mutate(self, info, id, nombre, run, direccion, website):
+
+        try:
+            UpdateEmpresa.validate_nombre(nombre)
+            UpdateEmpresa.validate_run(run)
+
+            empresa = Empresa.objects.get(pk=id)
+            empresa.nombre = nombre
+            empresa.save()
+
+            return UpdateEmpresa(empresa=empresa)
+        except Exception as e:  
+            raise Exception(f"Error al actualizar la empresa: {str(e)}")
 
 
 class Mutation(graphene.ObjectType):
